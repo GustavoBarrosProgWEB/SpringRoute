ec229fd229daf3d21476d00c7c72918dcebab4e2
Gustavo <gustavobarros.progweb@gmail.com>
2023-12-04 16:53:04 -0300

    Project created

---
+++ src/test/java/com/springroute/route/RouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springroute.route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
