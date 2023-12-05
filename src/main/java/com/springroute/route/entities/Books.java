e4d73c88a37aa2de5eacf9bc1bec08d1f69b1700
Gustavo <gustavobarros.progweb@gmail.com>
2023-12-04 21:52:39 -0300

    H2 set, entity Books created, database seeded

---
+++ src/main/java/com/springroute/route/entities/Books.java
@@ -0,0 +1,94 @@
+package com.springroute.route.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_books")
+public class Books {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Integer publishYear;
+    private String author;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public Books() {
+    }
+
+    public Books(Long id, String title, Integer publishYear, String author, String description) {
+        this.id = id;
+        this.title = title;
+        this.publishYear = publishYear;
+        this.author = author;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(Integer publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Books other = (Books) obj;
+        return Objects.equals(id, other.id);
+    }
+
+}
